8511f51be30bdadc84aae4afbd00a5b3eb195b23
taka-aka <ringo214ashi@gmail.com>
2025-10-05 20:55:21 +0900

    update next.js

---
+++ cyclingApp/Next.js/my-test-nextjs/src/app/api/weather/route.js
@@ -0,0 +1,11 @@
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const latitude = searchParams.get("lat");
+  const longitude = searchParams.get("lon");
+  const apiKey = "b8a18d623529ce3f03455646b1ad3c3b";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=ja`;
+
+  const res = await fetch(url);
+  const weather = await res.json();
+  return Response.json({ weather });
+}
--- cyclingApp/Next.js/my-test-nextjs/src/app/page.js
+++ cyclingApp/Next.js/my-test-nextjs/src/app/page.js
@@ -1,113 +1,59 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+
+export default function Page() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+
+  async function getWeather() {
+    const res = await fetch(`/api/weather?city=${city}`);
+    const data = await res.json();
+    setCity(data.weather.name);
+    setWeather(data.weather.weather[0].main);
+  }
+
+  function getGeolocation() {
+    //navigator.geolocationを呼び出すことでGeolocation APIを利用できる    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }
+
+  async function successCallback(position){
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    const res = await fetch(`/api/weather?lat=${latitude}&lon=${longitude}`);
+    const data = await res.json();
+    console.log(data);
+
+    setCity(data.weather.name);
+    setWeather(data.weather.weather[0].main);
+  };
+
+  function errorCallback(error){
+    alert("位置情報が取得できませんでした");
+    console.log(error);
+  };
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="text-center mt-8">
+      {/* <input
+        type="text"
+        placeholder="Enter city name"
+        className="border p-2 mr-3 mb-5"
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+        value={city}
+      />
+      <button className="bg-gray-200 p-2" onClick={getWeather}>
+        Get weather info
+      </button> */}
+      <button className="bg-gray-200 p-2" onClick={getGeolocation}>
+        現在地の天気を取得
+      </button>
+      <h1>City: {city}</h1>
+      <p>Weather: {weather}</p>
     </div>
   );
 }
 
-// export default function Home() {
-//   return (
-//     <main>
-//       <button onClick={() => alert('click')}>現在位置を取得する</button>
-//       <p>緯度：<span id = "latitude">???</span> <span>度</span></p>
-//       <p>経度：<span id = "longitude">???</span> <span>度</span></p>
-//     </main>
-//   );
-// }
