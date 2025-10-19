24efc214250dc7dce50f2c650d46b1d31e5a80a3
taka-aka <kaerukoichi@gmail.com>
2025-10-19 16:39:27 +0900

    色々機能を追加しました

--- Next.js/my-test-nextjs/src/app/api/weather_city/route.js
+++ Next.js/my-test-nextjs/src/app/api/weather_city/route.js
@@ -1,11 +1,10 @@
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
-  const latitude = searchParams.get("lat");
-  const longitude = searchParams.get("lon");
+  const city = searchParams.get("city");
   const apiKey = "b8a18d623529ce3f03455646b1ad3c3b";
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=ja`;
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ja`;
   const res = await fetch(url);
   const weather = await res.json();
+    console.log(weather)
   return Response.json({ weather });
 }
